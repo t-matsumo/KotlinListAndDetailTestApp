f099e84a7fb78bc52824e22bce8c369fbf83837c
t-matsumo <tatsukimatsumo@gmail.com>
2020-05-24 11:46:08 +0900

    Remove parcel

--- app/src/main/java/com/gmail/tatsukimatsumo/listanddetailtestapp/model/Article.kt
+++ app/src/main/java/com/gmail/tatsukimatsumo/listanddetailtestapp/model/Article.kt
@@ -1,35 +1,6 @@
 package com.gmail.tatsukimatsumo.listanddetailtestapp.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
 /**
  * 記事のデータ
  */
-data class Article(val id: String, val title: String, val contents: String): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(title)
-        parcel.writeString(contents)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Article> {
-        override fun createFromParcel(parcel: Parcel): Article {
-            return Article(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Article?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+data class Article(val id: String, val title: String, val contents: String)
